60f4abea2474b6838944ea0c884728290b3fbe5c
flavius75 <bicaadr@yahoo.com>
2024-02-09 00:07:45 +0100

    contact form

--- front/pages/contact/index.vue
+++ front/pages/contact/index.vue
@@ -1,6 +1,28 @@
 <script>
+import { ref } from "vue";
+
+// state
+const state = ref("");
 </script>
 
 <template>
-  Hello from contact page
+  <UCard>
+    <UForm :state="state" class="space-y-4">
+      <h1>Nous contacter</h1>
+
+      <UFormGroup label="Nom" name="name">
+        <UInput v-model="state" />
+      </UFormGroup>
+
+      <UFormGroup label="Email" name="email">
+        <UInput v-model="state" type="password" />
+      </UFormGroup>
+
+      <UFormGroup label="Message" name="message">
+        <UTextarea v-model="state" />
+      </UFormGroup>
+
+      <UButton type="submit"> Envoyer </UButton>
+    </UForm>
+  </UCard>
 </template>

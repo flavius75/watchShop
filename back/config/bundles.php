264cbbe36ff9a592321c98b1443090ed29ad143d
Flavius BICA <bicaadr@yahoo.com>
2024-03-03 22:54:54 +0100

    add swagger documantation

--- back/config/bundles.php
+++ back/config/bundles.php
@@ -9,4 +9,6 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
 ];
